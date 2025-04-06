fca297ebd8c2ff576f78a7fac5898a608b93dcf0
juma <nahshonjumaz@gmail.com>
2025-04-06 12:49:08 +0300

    timeout update on the mail

--- config/mail.php
+++ config/mail.php
@@ -42,7 +42,7 @@ return [
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
+            'timeout' => 1000*60,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
 
